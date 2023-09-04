b1fbe7465a52c88c5b2dbfc8de74767bdb84114d
JoergWolff <neuerfisch@wolffclan.de>
2023-09-04 10:38:40 +0200

    Create sout anomals

--- src/main/java/de/wolffclan/Main.java
+++ src/main/java/de/wolffclan/Main.java
@@ -4,8 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Animal zebra = new Animal(1,"Streifi","Horse",5);
+        Animal zebra2 = new Animal(1,"Streifi","Horse",5);
         Animal hippo = new Animal(2,"Hippopotamus","Horse",3);
         Animal aligator  = new Animal(3,"Kroko","Aligator",10);
 
+        System.out.println(zebra.toString());
+        System.out.println(zebra2.toString());
+        System.out.println(hippo.toString());
+        System.out.println(aligator.toString());
+        System.out.println("zebra1 is equal zebra2 : " +zebra.equals(zebra2));
+        System.out.println();
     }
 }
