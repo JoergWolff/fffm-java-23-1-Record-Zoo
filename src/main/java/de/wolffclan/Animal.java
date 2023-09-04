eb1f557cdc3ae1638af6c6a23f365af5fcb1a6cc
JoergWolff <neuerfisch@wolffclan.de>
2023-09-04 10:29:44 +0200

    Create Animals

---
+++ src/main/java/de/wolffclan/Animal.java
@@ -0,0 +1,10 @@
+package de.wolffclan;
+
+public record Animal(
+        int id,
+        String name,
+        String species,
+        int age
+) {
+
+}
---
+++ src/main/java/de/wolffclan/Main.java
@@ -0,0 +1,11 @@
+package de.wolffclan;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal zebra = new Animal(1,"Streifi","Horse",5);
+        Animal hippo = new Animal(2,"Hippopotamus","Horse",3);
+        Animal aligator  = new Animal(3,"Kroko","Aligator",10);
+
+    }
+}
